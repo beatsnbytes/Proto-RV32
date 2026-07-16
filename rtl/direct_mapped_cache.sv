bc6a7189e57fb6d189ce43b217e279d292faa244
beatsnbytes <vatkostalabros@hotmail.com>
2026-07-16 13:11:23 +0300

    feat: Cache stage 1 - Store support : Write through - Write allocate

--- rtl/direct_mapped_cache.sv
+++ rtl/direct_mapped_cache.sv
@@ -5,16 +5,20 @@ module direct_mapped_cache (
     input logic clk, 
     input logic rst,
     // CPU-side
+    input logic cpu_req_write, // Read = 0, Write = 1
     input logic cpu_req_valid, 
     output logic cpu_req_ready,
     input logic [31 : 0] cpu_addr,
+    input logic [31 : 0] cpu_wdata,
     output logic [31 : 0] cpu_resp_data,
     output logic cpu_resp_valid,
     input logic cpu_resp_ready,
     // Main memory side
+    output logic mem_req_write, // Read = 0, Write = 1
     output logic mem_req_valid,
     input logic mem_req_ready,
     output logic [31 : 0] mem_addr,
+    output logic [31 : 0] mem_wdata,
     input logic mem_resp_valid,
     output logic mem_resp_ready,
     input logic [127 : 0] mem_resp_data
@@ -38,16 +42,19 @@ module direct_mapped_cache (
 
 
     logic hit;
+    logic is_write_r;
+    logic [31 : 0] cpu_wdata_r;
  
     logic [TAG_WIDTH-1 : 0] tag_mem [CACHE_ENTRIES - 1 : 0];
     logic [DATA_LINE_WIDTH-1 : 0] data_mem [CACHE_ENTRIES - 1 : 0];
     logic [CACHE_ENTRIES - 1 : 0] valid_bit_mem;
 
-    typedef enum logic [1:0] {
-        IDLE = 2'b00,
-        M_SEND_REQ = 2'b01,
-        M_WAIT_RESP = 2'b10,
-        CPU_RESPOND = 2'b11
+    typedef enum logic [2:0] {
+        IDLE = 3'b000,
+        M_SEND_REQ = 3'b001,
+        M_WAIT_RESP = 3'b010,
+        WRITE = 3'b011,
+        CPU_RESPOND = 3'b100
     } state_t;
 
     state_t current_state, next_state;
@@ -65,9 +72,10 @@ module direct_mapped_cache (
     // Next state logic - combinational
     always_comb begin
         case (current_state)
-            IDLE : next_state = (cpu_req_valid) ? (hit ? CPU_RESPOND : M_SEND_REQ) : IDLE;
+            IDLE : next_state = (cpu_req_valid) ? (hit ? (cpu_req_write ? WRITE : CPU_RESPOND) : M_SEND_REQ) : IDLE;
             M_SEND_REQ : next_state = mem_req_ready ? M_WAIT_RESP : M_SEND_REQ;
-            M_WAIT_RESP : next_state = mem_resp_valid ? CPU_RESPOND : M_WAIT_RESP;
+            M_WAIT_RESP : next_state = mem_resp_valid ? (is_write_r ? WRITE : CPU_RESPOND) : M_WAIT_RESP;
+            WRITE : next_state = mem_req_ready ? CPU_RESPOND : WRITE;
             CPU_RESPOND: next_state = cpu_resp_ready ? IDLE : CPU_RESPOND; 
             default : next_state = current_state;
         endcase
@@ -91,6 +99,10 @@ module direct_mapped_cache (
         mem_resp_ready = 1'b0;
         cpu_resp_data = '0;
         mem_addr = '0;
+
+        mem_wdata = '0;
+        mem_req_write = 1'b0;
+
         if (!rst) begin
             case (current_state)
                 IDLE : begin
@@ -103,10 +115,19 @@ module direct_mapped_cache (
                 M_WAIT_RESP : begin
                     mem_resp_ready = 1'b1;
                 end
+                WRITE : begin
+                    mem_req_valid = 1'b1;
+                    mem_addr = cpu_addr_r;
+                    mem_wdata = cpu_wdata_r;
+                    mem_req_write = 1'b1;
+                end
                 CPU_RESPOND : begin
                     cpu_resp_valid = 1'b1;
-                    cpu_resp_data = data_mem[index_r][offset_r[OFFSET_WIDTH - 1 : OFFSET_WIDTH - 2]*32 +: 32];
+                    if (!is_write_r) begin
+                        cpu_resp_data = data_mem[index_r][offset_r[OFFSET_WIDTH - 1 : OFFSET_WIDTH - 2]*32 +: 32];
+                    end
                 end
+                default: ;
             endcase
         end
     end
@@ -115,8 +136,12 @@ module direct_mapped_cache (
     always_ff @(posedge clk) begin
         if (rst) begin
             cpu_addr_r <= '0;
+            is_write_r <= '0;
+            cpu_wdata_r <= '0;
         end else if ((current_state == IDLE) && (cpu_req_ready && cpu_req_valid)) begin
             cpu_addr_r <= cpu_addr;
+            is_write_r <= cpu_req_write;
+            cpu_wdata_r <= cpu_wdata;
         end
     end
 
@@ -127,15 +152,19 @@ module direct_mapped_cache (
                 data_mem[i] <= '0;
                 tag_mem[i] <= '0;
             end
-            valid_bit_mem <= '0;    
+            valid_bit_mem <= '0;   
         end else begin
             if ((current_state == M_WAIT_RESP) && mem_resp_valid) begin
                 data_mem[index_r] <= mem_resp_data;
                 tag_mem[index_r] <= tag_r;
                 valid_bit_mem[index_r] <= 1'b1;
             end
+            if (current_state == WRITE) begin
+                data_mem[index_r][offset_r[3:2]*32 +: 32] <= cpu_wdata_r;
+            end
         end
     end
 
 
 endmodule
+
